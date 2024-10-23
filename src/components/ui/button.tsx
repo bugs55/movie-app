8908bc2df5762cd6aed200dd020084871bca36ac
konrad2381 <konrad2381@gmail.com>
2024-10-23 11:05:02 +0000

    background

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         outline:
           "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         secondary:
-          "bg-movie-dark-lighter text-slate-100 hover:bg-movie-dark-lighter/70",
+          "bg-movie-dark-lighter/90 text-slate-100 hover:bg-movie-dark-lighter/60",
         ghost:
           "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
