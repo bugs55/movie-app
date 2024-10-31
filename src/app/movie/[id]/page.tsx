6895bc44139701e747a796de782627acf2608b65
konrad2381 <konrad2381@gmail.com>
2024-10-31 11:09:29 +0000

    moviePage as server component

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,24 +1,67 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { GetMovieDetails, WatchProviders } from "@/types/getMovieDetails.type";
 import axios from "axios";
 import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import useSWR from "swr";
-import type { ConfigUrlType } from "@/types/ConfigType.type";
+// import useSWR from "swr";
+import type { ConfigUrlType, ConfigType } from "@/types/ConfigType.type";
+import { notFound } from "next/navigation";
 
-type MoviePageParams = { params: { id: string } };
+type MoviePageParams = { params: Promise<{ id: string }> };
+type GetMovieDetailsResponse = GetMovieDetails &
+  ConfigUrlType & { watch_providers: WatchProviders["results"] };
 
-export default function MoviePage({ params }: MoviePageParams) {
-  const { id } = params;
+async function getMovieDetails(id: string): Promise<GetMovieDetailsResponse> {
+  const auth = `Bearer ${process.env.API_TOKEN}`;
+  const data = await axios
+    .get<GetMovieDetails>(`https://api.themoviedb.org/3/movie/${id}`, {
+      headers: { Authorization: auth },
+    })
+    .then((res) => res.data);
 
-  const fetcher = (url: string) =>
-    axios.post(url, { id }).then((res) => res.data);
-  const { data, isLoading, error } = useSWR<
-    GetMovieDetails & ConfigUrlType & { watch_providers: WatchProviders }
-  >("/api/getMovieDetails", fetcher);
+  const config = await axios
+    .get<ConfigType>("https://api.themoviedb.org/3/configuration", {
+      headers: { Authorization: auth },
+    })
+    .then((res) => ({
+      secure_base_url: res.data.images.secure_base_url,
+      base_url: res.data.images.base_url,
+    }));
+
+  const watchProviders = await axios
+    .get<WatchProviders>(
+      `https://api.themoviedb.org/3/movie/${id}/watch/providers`,
+      {
+        headers: { Authorization: auth },
+      }
+    )
+    .then((res) => res.data);
+
+  return {
+    ...data,
+    ...config,
+    watch_providers: watchProviders.results,
+  };
+}
+
+export default async function MoviePage({ params }: MoviePageParams) {
+  const id = (await params).id;
+  let data: GetMovieDetailsResponse | undefined;
+
+  try {
+    const res = await getMovieDetails(id);
+    data = res;
+  } catch (err) {
+    console.error("getMovieDetails failed", err);
+    notFound();
+  }
+
+  // const fetcher = (url: string) =>
+  //   axios.post(url, { id }).then((res) => res.data);
+  // const { data, isLoading, error } = useSWR<
+  //   GetMovieDetails & ConfigUrlType & { watch_providers: WatchProviders }
+  // >("/api/getMovieDetails", fetcher);
 
   return (
     <div className="px-5">
