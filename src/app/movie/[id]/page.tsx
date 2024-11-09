c627c15168547451fb1798de5d3051ddf449ff6d
Konrad <konrad2381@gmail.com>
2024-11-09 13:54:03 +0100

    movie info & cast on movie page

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -8,6 +8,8 @@ import Link from "next/link";
 // import useSWR from "swr";
 import type { ConfigUrlType, ConfigType } from "@/types/ConfigType.type";
 import { notFound } from "next/navigation";
+import { Badge } from "@/components/ui/badge";
+import { CreditsResponse } from "@/types/CreditsType.type";
 
 const Button = dynamic(() =>
   import("@/components/ui/button").then((m) => m.Button)
@@ -21,7 +23,10 @@ const Rocket = dynamic(() => import("lucide-react").then((m) => m.Rocket));
 
 type MoviePageParams = { params: Promise<{ id: string }> };
 type GetMovieDetailsResponse = GetMovieDetails &
-  ConfigUrlType & { watch_providers: WatchProviders["results"] };
+  ConfigUrlType & {
+    watch_providers: WatchProviders["results"];
+    credits: CreditsResponse;
+  };
 
 async function getMovieDetails(id: string): Promise<GetMovieDetailsResponse> {
   const auth = `Bearer ${process.env.API_TOKEN}`;
@@ -49,10 +54,17 @@ async function getMovieDetails(id: string): Promise<GetMovieDetailsResponse> {
     )
     .then((res) => res.data);
 
+  const credits = await axios
+    .get<CreditsResponse>(`https://api.themoviedb.org/3/movie/${id}/credits`, {
+      headers: { Authorization: auth },
+    })
+    .then((res) => res.data);
+
   return {
     ...data,
     ...config,
     watch_providers: watchProviders.results,
+    credits,
   };
 }
 
@@ -101,13 +113,13 @@ export default async function MoviePage({ params }: MoviePageParams) {
           )}
           <div className="absolute p-5 top-0 left-0 w-full h-full bg-gradient-to-t from-black/70">
             <div className="flex flex-col justify-end h-full">
-              <div className="md:text-5xl text-2xl font-bold h-fit">
+              <div className="md:text-5xl text-2xl font-bold h-fit sm:block hidden">
                 {data.title}
               </div>
-              <div className="max-w-[700px] md:mt-4 mt-2 text-neutral-300">
+              <div className="max-w-[700px] md:mt-4 mt-2 text-neutral-300 sm:block hidden">
                 {data.overview}
               </div>
-              <div className="flex items-center gap-4">
+              <div className="flex items-center gap-2">
                 <div className="mt-3 w-fit px-3 py-2 flex items-center gap-2 backdrop-blur bg-black/40 rounded-md">
                   <Star color="#FFDE4D" size={20} />
                   <div className="text-base font-semibold">
@@ -115,13 +127,13 @@ export default async function MoviePage({ params }: MoviePageParams) {
                   </div>
                 </div>
                 <div className="mt-3 w-fit px-3 py-2 flex items-center gap-2 backdrop-blur bg-black/40 rounded-md">
-                  <Users color="#ff7070" size={20} />
+                  <Users color="#FFDE4D" size={20} />
                   <div className="text-base font-semibold">
                     {data.vote_count}
                   </div>
                 </div>
                 <div className="mt-3 w-fit px-3 py-2 flex items-center gap-2 backdrop-blur bg-black/40 rounded-md">
-                  <Rocket color="#0ea5e9" size={20} />
+                  <Rocket color="#FFDE4D" size={20} />
                   <div className="text-base font-semibold">
                     {getYear(data.release_date)}
                   </div>
@@ -129,6 +141,80 @@ export default async function MoviePage({ params }: MoviePageParams) {
               </div>
             </div>
           </div>
+        </div>
+        <div className="mt-5">
+          <div className="md:text-5xl text-2xl font-bold h-fit sm:hidden block">
+            {data.title}
+          </div>
+          <div className="md:mt-4 mt-2 text-neutral-300 sm:hidden block">
+            {data.overview}
+          </div>
+          {data.genres ? (
+            <div className="mt-3">
+              <div className="text-lg font-bold">Genres</div>
+              <div className="flex flex-wrap gap-2 mt-1">
+                {data.genres.map((genre) => (
+                  <Badge className="text-sm" key={genre.id}>
+                    {genre.name}
+                  </Badge>
+                ))}
+              </div>
+            </div>
+          ) : null}
+          {data.runtime ? (
+            <div className="mt-3">
+              <div className="text-lg font-bold">Runtime</div>
+              <div className="flex flex-wrap gap-2">
+                {Math.floor(data.runtime / 60)}h {data.runtime % 60}m
+              </div>
+            </div>
+          ) : null}
+          {data.spoken_languages ? (
+            <div className="mt-3">
+              <div className="text-lg font-bold">Spoken languages</div>
+              <div className="flex flex-wrap gap-2 mt-1">
+                {data.spoken_languages.map((language) => (
+                  <Badge className="text-sm" key={language.english_name}>
+                    {language.english_name}
+                  </Badge>
+                ))}
+              </div>
+            </div>
+          ) : null}
+          {data.production_countries ? (
+            <div className="mt-3">
+              <div className="text-lg font-bold">Production countries</div>
+              <div className="flex flex-wrap gap-2 mt-1">
+                {data.production_countries.map((country) => (
+                  <Badge className="text-sm" key={country.name}>
+                    {country.name}
+                  </Badge>
+                ))}
+              </div>
+            </div>
+          ) : null}
+          {data.credits.cast && data ? (
+            <div className="mt-3">
+              <div className="text-lg font-bold">Cast</div>
+              <div className="flex flex-wrap gap-2 mt-1">
+                {data.credits.cast.map((cast) => (
+                  <div>
+                    <Image
+                      src={`${data.secure_base_url}w500${cast.profile_path}`}
+                      alt={cast.name}
+                      width={500}
+                      height={500}
+                      className="rounded-full w-20 h-20 object-cover"
+                    />
+                    <div className="text-sm">{cast.name}</div>
+                    <div className="text-xs text-neutral-400">
+                      {cast.character}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
